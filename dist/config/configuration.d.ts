2077715bf3072cd12ca0bdf9899bb8a5923e57ac
Neider Fabian Walteros Pinto <31144689+NeFaWaltPint@users.noreply.github.com>
2022-02-13 10:26:55 -0500

    Delete dist directory

--- dist/config/configuration.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const environment: {
-    USER: string;
-    PASS: string;
-    HOST: string;
-    PORT: string;
-    DB: string;
-    HTTP_PORT: string;
-    AUTH_DB: string;
-};
--- dist/filters/dto/filter.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class FilterDto {
-    readonly query: string;
-    readonly fields: string;
-    readonly sortby: string;
-    readonly order: string;
-    readonly limit: string;
-    readonly offset: string;
-    readonly populate: string;
-}
