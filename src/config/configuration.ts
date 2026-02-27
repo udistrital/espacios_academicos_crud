51c3df04a616b4f1efc8fa9ff02e3fbfda7b60f3
Alvaro Zarabanda <alvaroalejo25@gmail.com>
2026-02-27 14:22:38 -0500

    cicd: actualizacion lineamientos espacios_academicos_crud udistrital/sga_documentacion#664
    se ajusta cicd para api nest

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,11 @@
-export const environment =  {
+export const environment = {
     USER: process.env.ESPACIOS_ACADEMICOS_CRUD_USER,
     PASS: process.env.ESPACIOS_ACADEMICOS_CRUD_PASS,
     HOST: process.env.ESPACIOS_ACADEMICOS_CRUD_HOST,
     PORT: process.env.ESPACIOS_ACADEMICOS_CRUD_PORT,
     DB: process.env.ESPACIOS_ACADEMICOS_CRUD_DB,
     HTTP_PORT: process.env.ESPACIOS_ACADEMICOS_CRUD_HTTP_PORT,
-    AUTH_DB: process.env.ESPACIOS_ACADEMICOS_CRUD_AUTH_DB 
+    AUTH_DB: process.env.ESPACIOS_ACADEMICOS_CRUD_AUTH_DB,
+    RUNMODE: process.env.ESPACIOS_ACADEMICOS_CRUD_RUNMODE || 'prod',
+    PARAMETER_STORE: process.env.PARAMETER_STORE
 };
